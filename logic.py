b29e3618c4200cfb026a99df90b3e890c4da5232
ananya09 <ananyamengistu06@gmail.com>
2026-02-08 23:26:19 -0800

    firt oner

---
+++ logic.py
@@ -0,0 +1,12 @@
+while True:
+    print("welcome to daily_life.csv")
+    print("------------------------------")
+    print("1.Add daily entry")
+    print("2.View all entries")
+    print("3.Show analysis")
+    print("4.exit")
+    print("------------------------------")
+    choice=int(input("enter your choose: "))
+    if choice==4:
+        print("goodbye......")
+        break
